aa688b9d9bf90cc2a82edca21daad0a526111bcc
Anant Narayan <anantnrg@gmail.com>
2025-08-17 21:28:46 +0530

    Cleanup

--- ciphers/src/bin/substitution.rs
+++ ciphers/src/bin/substitution.rs
@@ -3,13 +3,13 @@ use ciphers::substitution::Substitution;
 fn main() {
     let cipher = Substitution::new("KRYPTOS".to_string());
 
-    let string = "FOUR TANGOS NORTHWEST CLEARED TO ENGAGE".to_string();
+    let string = "FOUR TANGOS NORTHWEST. CLEARED TO ENGAGE";
 
     let encoded = cipher.encode(string);
 
     println!(
         "Encoded: {}\nDecoded: {}",
         encoded.clone(),
-        cipher.decode(encoded)
+        cipher.decode(encoded.as_str())
     );
 }
--- ciphers/src/substitution/mod.rs
+++ ciphers/src/substitution/mod.rs
@@ -1,6 +1,7 @@
 use super::{ALPHABET, remove_duplicate_chars};
 use std::collections::HashMap;
 
+#[derive(Debug)]
 pub struct Substitution {
     pub key: String,
     pub enc_map: HashMap<char, char>,
@@ -13,9 +14,7 @@ impl Substitution {
         let alphabet = ALPHABET.to_string();
         let mut sub_alphabets: Vec<char> = vec![];
 
-        for c in key.chars() {
-            sub_alphabets.push(c);
-        }
+        sub_alphabets.extend(key.chars());
 
         for c in alphabet.chars() {
             if !key.contains(c) {
@@ -26,11 +25,9 @@ impl Substitution {
         let mut enc_map: HashMap<char, char> = HashMap::new();
         let mut dec_map: HashMap<char, char> = HashMap::new();
 
-        let alphabet_vec = alphabet.chars().collect::<Vec<_>>();
-
-        for (i, c) in alphabet_vec.iter().enumerate() {
-            enc_map.insert(*c, sub_alphabets[i]);
-            dec_map.insert(sub_alphabets[i], *c);
+        for (i, c) in alphabet.chars().enumerate() {
+            enc_map.insert(c, sub_alphabets[i]);
+            dec_map.insert(sub_alphabets[i], c);
         }
 
         Substitution {
@@ -40,13 +37,12 @@ impl Substitution {
         }
     }
 
-    pub fn encode(&self, string: String) -> String {
-        let enc_map = &self.enc_map;
+    pub fn encode(&self, string: &str) -> String {
         let mut enc_string = String::new();
 
-        for c in string.to_ascii_uppercase().chars() {
-            if enc_map.contains_key(&c) {
-                enc_string.push(*enc_map.get(&c).unwrap());
+        for c in string.chars().map(|c| c.to_ascii_uppercase()) {
+            if let Some(&enc_c) = self.enc_map.get(&c) {
+                enc_string.push(enc_c);
             } else {
                 enc_string.push(c);
             }
@@ -55,13 +51,12 @@ impl Substitution {
         enc_string
     }
 
-    pub fn decode(&self, encoded: String) -> String {
-        let dec_map = &self.dec_map;
+    pub fn decode(&self, encoded: &str) -> String {
         let mut dec_string = String::new();
 
-        for c in encoded.to_ascii_uppercase().chars() {
-            if dec_map.contains_key(&c) {
-                dec_string.push(*dec_map.get(&c).unwrap());
+        for c in encoded.chars().map(|c| c.to_ascii_uppercase()) {
+            if let Some(&dec_c) = self.dec_map.get(&c) {
+                dec_string.push(dec_c);
             } else {
                 dec_string.push(c);
             }
