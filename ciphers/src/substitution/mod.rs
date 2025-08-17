a0c928b6472d70b38fd4eed0be3473446742ed9f
Anant Narayan <anantnrg@gmail.com>
2025-08-17 18:16:37 +0530

    start working on substitution cipher

--- ciphers/src/substitution/mod.rs
+++ ciphers/src/substitution/mod.rs
@@ -2,5 +2,6 @@ use std::collections::HashMap;
 
 pub struct Substitution {
     pub key: String,
-    pub alphabet: Option<HashMap<char, char>>,
+    pub enc_map: HashMap<char, char>,
+    pub dec_map: HashMap<char, char>,
 }
